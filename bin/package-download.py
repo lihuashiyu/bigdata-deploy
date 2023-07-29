39e2894064de36bd82155a7378ffd44c5fa52854
Administrator <lihuashiyu@github.com>
2023-07-29 21:08:52 +0800

    添加 AliYun 镜像软件包爬取

---
+++ bin/package-download.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+====================================================================================================
+    ProjectName   ：  bigdata-deploy
+    FileName      ：  package-download
+    CreateTime    ：  2023-07-29 20:59:36
+    Author        ：  lihua_shiyu
+    Email         ：  lihuashiyu@github.com
+    PythonCompiler：  3.9.13
+    Description   ：  下载软件包，以及 爬取 AliYun 镜像的 epel 等 rpm 包
+====================================================================================================
+"""
+
+import requests
+import os.path
+
+
+if __name__ == '__main__':
+    print("Hello World!")
+    pass
