3186ee915dd47e9c752ef2c752185d909d07c99e
lihuashiyu <lihuashiyu@github.com>
2023-08-15 23:48:45 +0800

    更新 Flink 启停脚本

--- script/apache/flink.sh
+++ script/apache/flink.sh
@@ -11,105 +11,224 @@
     
     
 FLINK_HOME=$(cd -P "$(dirname "$(readlink -e "$0")")/../" || exit; pwd -P)     # Flink 安装目录
-SERVICE_NAME=org.apache.flink
 ALIAS_NAME=Flink                                                               # 服务别名
 
-CLUSTER_ENTRY_PORT=8082
-RPC_PORT=6123
-REST_PORT=8083
-WEB_PORT=9082
+CLUSTER_ENTRY_PORT=8082                                                        # Master 占用端口号
+RPC_PORT=6123                                                                  # Master RPC  通信端口号
+REST_PORT=8083                                                                 # Master REST 通信端口号
+WEB_PORT=9082                                                                  # 外部访问 Master  WebUI 端口号
 
-CLUSTER_ENTRY=org.apache.flink.runtime.entrypoint.StandaloneSessionClusterEntrypoint
-TASK_MANAGER=org.apache.flink.runtime.taskexecutor.TaskManagerRunner
+CLUSTER_ENTRY=org.apache.flink.runtime.entrypoint.StandaloneSessionClusterEntrypoint     # Flink Master 进程名称
+TASK_MANAGER=org.apache.flink.runtime.taskexecutor.TaskManagerRunner           # Flink TaskManager 端口号
+HISTORY_SERVER=org.apache.flink.runtime.webmonitor.history.HistoryServer       # Flink 历史服务器进程名称
+
+MASTER_LIST=(${master_list})                                                           # JobManager    主机主机名
+SLAVER_LIST=(${slaver_list})                                          # TaskManager   主机主机名
+HISTORY_LIST=(${history_list})                                                          # HistoryServer 主机主机名
+FLINK_LOG_FILE="flink-$(date +%F).log"                                         # Flink   程序操作日志文件
+HISTORY_LOG_FILE="history-$(date +%F).log"                                     # History 程序操作日志文件
+USER=$(whoami)                                                                 # 获取当前登录用户
+RUNNING=1                                                                      # 服务运行状态码
+STOP=0                                                                         # 服务停止状态码
+
+
+# 服务状态检测
+function service_status()
+{
+    # 1. 定义变量
+    local host_name result_list=() pid_list=() master_pid slaver_pid history_pid run_pid_count
+        
+    # 2. 遍历 Master 的所有的主机，查看 jvm 进程
+    for host_name in "${MASTER_LIST[@]}"
+    do
+        # 2.1 获取程序 Master 的 pid 数量
+        master_pid=$(ssh "${USER}@${host_name}" "source ~/.bashrc; source /etc/profile; ps -aux | grep -i '${USER}' | grep -viE 'grep|$0' | grep -ci '${CLUSTER_ENTRY}'")
+        
+        # 2.2 判断该主机的 pid 是否存在
+        if [ "${master_pid}" -ne 1 ]; then
+            result_list[${#result_list[@]}]="主机（${host_name}）的程序（JobManager）出现错误"
+            pid_list[${#pid_list[@]}]="${STOP}"
+        else
+            pid_list[${#pid_list[@]}]="${RUNNING}"
+        fi
+    done
+    
+    # 3. 遍历 Slaver 的所有的主机，查看 jvm 进程
+    for host_name in "${SLAVER_LIST[@]}"
+    do
+        # 3.1 程序 Slaver 的 pid 数量
+        slaver_pid=$(ssh "${USER}@${host_name}" "source ~/.bashrc; source /etc/profile; ps -aux | grep -i '${USER}' | grep -viE 'grep|$0' | grep -ci '${TASK_MANAGER}'")
+        
+        # 3.2 判断该主机的 pid 是否存在
+        if [ "${slaver_pid}" -ne 1 ]; then
+            result_list[${#result_list[@]}]="主机（${host_name}）的程序（TaskManager）出现错误"
+            pid_list[${#pid_list[@]}]="${STOP}"
+        else
+            pid_list[${#pid_list[@]}]="${RUNNING}"
+        fi
+    done
+    
+    # 4. 遍历 History 的所有的主机，查看 jvm 进程
+    for host_name in "${HISTORY_LIST[@]}"
+    do
+        # 4.1 程序 HistoryServer 的 pid 数量
+        history_pid=$(ssh "${USER}@${host_name}" "source ~/.bashrc; source /etc/profile; ps -aux | grep -i '${USER}' | grep -viE 'grep|$0' | grep -ci '${HISTORY_SERVER}'")
+        
+        # 4.2 判断该主机的 pid 是否存在
+        if [ "${server_2_pid}" -ne 1 ]; then
+            result_list[${#result_list[@]}]="主机（${host_name}）的程序（HiveServer2）出现错误"
+            pid_list[${#pid_list[@]}]="${STOP}"
+        else
+            pid_list[${#pid_list[@]}]="${RUNNING}"
+        fi
+    done
+      
+    # 5. 判断是否所有的进程都正常
+    run_pid_count=$(echo "${pid_list[@]}"  | grep -ci "${RUNNING}")
+    
+    if [ "${#result_list[@]}" -eq 0 ]; then
+        echo "${RUNNING}"
+    elif [ "${run_pid_count}" -eq 0 ]; then
+        echo "${STOP}"
+    else
+        echo "${result_list[@]}"
+    fi
+}
+
+
+# 启动服务
+function service_start()    
+{
+    # 1. 定义局部变量
+    local status ps
+    
+    # 2. 判断程序所处的状态
+    status=$(service_status)
+    
+    # 3.1 若程序没有运行，则运行程序，否则打印程序运行状态
+    if [ "${status}" == "${STOP}" ]; then
+        echo "    程序（${ALIAS_NAME}）正在加载 ......"
+        
+        # 3.2 启动 Flink 集群
+        "${FLINK_HOME}/bin/start-cluster.sh"  >> "${FLINK_HOME}/logs/${FLINK_LOG_FILE}" 2>&1
+        
+        echo "    程序（${ALIAS_NAME}）启动验证中 ......"
+        
+        # 3.3 启动 Flink 的历史服务器
+        "${FLINK_HOME}/bin/bin/historyserver.sh" start  >> "${FLINK_HOME}/logs/${HISTORY_LOG_FILE}" 2>&1
+        sleep 2
+        
+        # 3.4 判断所有程序启动是否成功
+        status=$(service_status)
+        if [ "${status}" == "${RUNNING}" ]; then
+            echo "    程序（${ALIAS_NAME}）启动成功 ...... "
+        else
+            echo "    程序（${ALIAS_NAME}）启动失败 ...... "
+            for ps in ${status}
+            do
+                echo "    ${ps} ...... "
+            done
+        fi
+    elif [ "${status}" == "${RUNNING}" ]; then
+        echo "    程序（${ALIAS_NAME}）正在运行 ...... "
+    else
+        echo "    程序（${ALIAS_NAME}）运行出错 ...... "
+        for ps in ${status}
+        do
+            echo "    ${ps} ...... "
+        done
+    fi
+}
+
+
+# 停止服务
+function service_stop()
+{
+    # 1. 定义局部变量
+    local status ps
+
+    # 2. 判断程序所处的状态
+    status=$(service_status)
+    
+    # 3.1 若程序正在运行，则停止程序，否则打印程序运行状态
+    if [ "${status}" == "${STOP}" ]; then
+        echo "    程序（${ALIAS_NAME}）已经停止 ...... "
+    elif [ "${status}" == "${RUNNING}" ]; then
+        echo "    程序（${ALIAS_NAME}）正在停止 ......"
+        
+        # 3.2 关闭 Flink 的历史服务器
+        "${FLINK_HOME}/bin/historyserver.sh" stop  >> "${FLINK_HOME}/logs/${HISTORY_LOG_FILE}" 2>&1
+        
+        echo "    程序（${ALIAS_NAME}）停止验证中 ......"
+        
+        # 3.3 关闭 Flink 集群
+        "${FLINK_HOME}/bin/stop-cluster.sh"  >> "${FLINK_HOME}/logs/${FLINK_LOG_FILE}" 2>&1
+        sleep 3
+        
+        # 3.4 判断所有程序是否关闭成功
+        status=$(service_status)
+        if [ "${status}" == "${STOP}" ]; then
+            echo "    程序（${ALIAS_NAME}）关闭成功 ...... "
+        else
+            echo "    程序（${ALIAS_NAME}）关闭失败 ...... "
+            for ps in ${status}
+            do
+                echo "    ${ps} ...... "
+            done
+        fi            
+    else
+        echo "    程序（${ALIAS_NAME}）运行出错 ...... "
+        for ps in ${status}
+        do
+            echo "    ${ps} ...... "
+        done
+    fi    
+}
 
 
 printf "\n================================================================================\n"
-#  匹配输入参数
-case "$1" in
-    #  1. 运行程序
-    start)
-        # 1.1 查找程序的 pid
-        pid_list=$(ps -aux | grep -i ${SERVICE_NAME} | grep -v grep | awk '{print $2}' | awk -F "_" '{print $1}')
-        
-        #  1.2 若 pid 不存在，则运行程序，否则打印程序运行状态
-        if [ ! "${pid_list}" ]; then
-            echo "    程序 ${ALIAS_NAME} 正在加载中 ......"
-            "${FLINK_HOME}/bin/start-cluster.sh" > /dev/null 2>&1
-            sleep 2
-            
-            # 1.3 判断程序 StandaloneSessionClusterEntrypoint 启动是否成功
-            cluster_pid=$(ps -aux | grep -i ${CLUSTER_ENTRY} | grep -v grep | awk '{print $2}' | awk -F "_" '{print $1}')
-            if [ ! "${cluster_pid}" ]; then
-                echo "    程序 StandaloneSessionClusterEntrypoint 启动失败 ...... "
-            fi
-            
-            # 1.4 判断程序 TaskManagerRunner 启动是否成功
-            task_pid=$(ps -aux | grep -i ${TASK_MANAGER} | grep -v grep | awk '{print $2}' | awk -F "_" '{print $1}')
-            if [ ! "${task_pid}" ]; then
-                echo "    程序 TaskManagerRunner 启动失败 ...... "
-            fi
-            
-            # 1.5 判断所有程序启动是否成功
-            pid_count=$(ps -aux | grep -i ${SERVICE_NAME} | grep -v grep | awk '{print $2}' | awk -F "_" '{print $1}' | wc -l)
-            if [ "${pid_count}" -ge 2 ]; then
-                echo "    程序 ${ALIAS_NAME} 启动成功 ...... "
-            else
-                echo "    程序 ${ALIAS_NAME} 启动失败 ...... "
-            fi
-            
-        else
-            echo "    程序 ${ALIAS_NAME} 正在运行当中 ...... "
-        fi
-    ;;
-    
-    
-    #  2. 停止
-    stop)
-        # 2.1 根据程序的 pid 查询程序运行状态
-        pid_count=$(ps -aux | grep -i ${SERVICE_NAME} | grep -v grep | awk '{print $2}' | awk -F "_" '{print $1}' | wc -l)
-        if [ "${pid_count}" -eq 0 ]; then
-            echo "    ${ALIAS_NAME} 的进程不存在，程序没有运行 ...... "
-        elif [ "${pid_count}" -eq 2 ]; then
-            # 2.2 杀死进程，关闭程序
-            "${FLINK_HOME}/bin/stop-cluster.sh" > /dev/null 2>&1
-            sleep 1
+# 1. 获取脚本执行开始时间
+start=$(date -d "$(date +"%Y-%m-%d %H:%M:%S")" +%s)
 
-            # 2.3 若还未关闭，则强制杀死进程，关闭程序
-            pid_count=$(ps -aux | grep -i ${SERVICE_NAME} | grep -v grep | awk '{print $2}' | wc -l)
-            if [ "${pid_count}" -ge 1 ]; then
-                temp=$(ps -aux | grep -i ${SERVICE_NAME} | grep -v grep | awk '{print $2}' | xargs kill -9)
-            fi
-            
-            echo "    程序 ${ALIAS_NAME} 已经停止成功 ......"            
-        else
-            echo "    程序 ${ALIAS_NAME} 运行出现问题 ......"
-        fi
+# 2. 匹配输入参数
+case "$1" in
+    # 2.1 运行程序
+    start)
+        service_start
     ;;
     
+    # 2.2 停止程序
+    stop)
+        service_stop
+    ;;
     
-    #  3. 状态查询
+    # 2.3 状态查询
     status)
-        # 3.1 查看正在运行程序的 pid
-        pid_count=$(ps -aux | grep -i ${SERVICE_NAME} | grep -v grep | awk '{print $2}' | awk -F "_" '{print $1}' | wc -l)
-        #  3.2 判断 ES 运行状态
-        if [ "${pid_count}" -eq 0 ]; then
-            echo "    程序 ${ALIAS_NAME} 已经停止 ...... "
-        elif [ "${pid_count}" -eq 2 ]; then
-            echo "    程序 ${ALIAS_NAME} 正在运行中 ...... "
+        # 2.3.1 判断程序所处的状态
+        status=$(service_status)
+        
+        # 2.3.2 判断程序运行状态
+        if [ "${status}" == "${STOP}" ]; then
+            echo "    程序（${ALIAS_NAME}）已经停止 ...... "
+        elif [ "${status}" == "${RUNNING}" ]; then
+            echo "    程序（${ALIAS_NAME}）正在运行 ...... "
         else
-            echo "    程序 ${ALIAS_NAME} 运行出现问题 ...... "
+            echo "    程序（${ALIAS_NAME}）运行出错 ...... "
+            for ps in ${status}
+            do
+                echo "    ${ps} ...... "
+            done
         fi
     ;;
     
-    
-    #  4. 重启程序
+    # 2.4 重启程序
     restart)
-        "$0" stop
-        "$0" start
+        service_stop
+        sleep 3
+        service_start
     ;;
     
-    
-    #  5. 其它情况
+    # 2.5 其它情况
     *)
         echo "    脚本可传入一个参数，如下所示：              "
         echo "        +-----------------------------------+ "
@@ -122,5 +241,14 @@ case "$1" in
         echo "        +-----------------------------------+ "
     ;;
 esac
-printf "================================================================================\n\n"
 
+# 3. 获取脚本执行结束时间
+end=$(date -d "$(date +"%Y-%m-%d %H:%M:%S")" +%s)
+
+# 4. 计算并输出脚本执束时间
+if [ "$#" -eq 1 ]  && { [ "$1" == "start" ] || [ "$1" == "stop" ] || [ "$1" == "restart" ]; }; then
+    echo "    脚本（$(basename "$0")）执行共消耗：$(( end - start ))s ...... "
+fi
+
+printf "================================================================================\n\n"
+exit 0
