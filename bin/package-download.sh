f7cb89999e2a6db11118f71a1928e0568a809248
Administrator <lihuashiyu@github.com>
2023-08-26 18:47:46 +0800

    更新 软件下载 脚本

--- bin/package-download.sh
+++ bin/package-download.sh
@@ -14,43 +14,44 @@ ROOT_DIR=$(cd "${SERVICE_DIR}/../" || exit; pwd)                               #
 CONFIG_FILE="server.conf"                                                      # 配置文件名称
 LOG_FILE="package-download-$(date +%F).log"                                    # 程序操作日志文件
 # 定义所需要的下载的软件包地址
-software_list=(mysql redis java python scala maven gradle hadoop spark.nohadoop)
+software_list=(java python scala maven gradle mysql redis nginx hadoop spark flink kafka zookeeper hive hbase phoenix doris)
 
 
 # 刷新环境变量
 function flush_env()
-{
-    mkdir -p "${ROOT_DIR}/logs"                                                # 创建日志目录
-    mkdir -p "${ROOT_DIR}/package"                                                # 创建日志目录
+{    
+    mkdir -p "${ROOT_DIR}/logs"  "${ROOT_DIR}/package"                         # 创建日志目录和包下载目录
     
     echo "    ************************** 刷新环境变量 **************************    "
+    # 判断用户环境变量文件是否存在
     if [ -e "${HOME}/.bash_profile" ]; then
-        source "${HOME}/.bash_profile"
+        source "${HOME}/.bash_profile"                                         # RedHat 用户环境变量文件
     elif [ -e "${HOME}/.bashrc" ]; then
-        source "${HOME}/.bashrc"
+        source "${HOME}/.bashrc"                                               # Debian、RedHat 用户环境变量文件
     fi
     
-    source "/etc/profile"
+    source "/etc/profile"                                                      # 系统环境变量文件路径
     
     echo "    ************************** 获取公共函数 **************************    "
     # shellcheck source=./common.sh
-    source "${ROOT_DIR}/bin/common.sh"
+    source "${ROOT_DIR}/bin/common.sh"                                         # 当前程序使用的公共函数
     
-    export -A PARAM_LIST=()
-    read_param "${ROOT_DIR}/conf/${CONFIG_FILE}"
+    export -A PARAM_LIST=()                                                    # 初始化 配置文件 参数
+    read_param "${ROOT_DIR}/conf/${CONFIG_FILE}"                               # 读取配置文件，获取参数    
 }
 
 
 # 下载软件包（$1：配置文件中软件包 url 的 key）
 function download()
 {
-    # 定义局部变量
-    local url file_name 
+    local url file_name                                                        # 定义局部变量
+    url=$(get_param "$1")                                                      # 软件下载的 url
     
-    url=$(get_param "$1")
+    # 下载软件包
     if [[ -n ${url} ]]; then
+        file_name=$(echo "${url}" | sed 's/.*\/\([^\/]*\)$/\1/')               # 获取文件名
+        
         # 查看安装包是否存在，存在就删除
-        file_name=$(echo "${url}" | sed 's/.*\/\([^\/]*\)$/\1/')
         if [[ -f "${ROOT_DIR}/package/${file_name}" ]]; then
             rm -rf "${ROOT_DIR}/package/${file_name}"
         fi
@@ -65,9 +66,8 @@ function download()
 
 
 printf "\n================================================================================\n" 
-if [ "$#" -gt 0 ]; then
-    flush_env                                                                  # 刷新环境变量
-fi
+start=$(date -d "$(date +"%Y-%m-%d %H:%M:%S")" +%s)                            # 获取脚本执行开始时间
+flush_env                                                                      # 刷新环境变量
 
 # 判断脚本是否传入参数，未传入会使用自定义参数
 if [ "$#" -eq 0 ]; then
@@ -81,6 +81,9 @@ else
         download "${file}.url"
     done
 fi
+
+end=$(date -d "$(date +"%Y-%m-%d %H:%M:%S")" +%s)                              # 获取脚本执行的结束时间
+echo "    脚本（$(basename "$0")）执行共消耗：$(( end - start ))s ...... "
 
 printf "================================================================================\n\n"
 exit 0
