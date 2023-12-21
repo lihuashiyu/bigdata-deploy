b25c1fc816032ff5b64869e8c337c33ac1cb5ebd
lihuashiyu <lihuashiyu@github.com>
2023-12-21 22:28:29 +0800

    添加 SSH 和 SCP 端口号配置

--- bin/password-free-login.sh
+++ bin/password-free-login.sh
@@ -13,6 +13,8 @@ SERVICE_DIR=$(dirname "$(readlink -e "$0")")                                   #
 ROOT_DIR=$(cd "${SERVICE_DIR}/../" || exit; pwd)                               # 项目根目录
 CONFIG_FILE="server.conf"                                                      # 配置文件名称
 LOG_FILE="password-free-login-$(date +%F).log"                                 # 程序操作日志文件
+SSH_PORT="22"                                                                  # SSH 端口号
+SCP_PORT="${SSH_PORT}"                                                         # SCP 端口号
 USER=$(whoami)                                                                 # 当前使用的用户
 
 
@@ -22,20 +24,21 @@ function flush_env()
     mkdir -p "${ROOT_DIR}/logs"                                                # 创建日志目录
     
     echo "    ************************** 刷新环境变量 **************************    "
+    # 判断用户环境变量文件是否存在
     if [ -e "${HOME}/.bash_profile" ]; then
-        source "${HOME}/.bash_profile"
+        source "${HOME}/.bash_profile"                                         # RedHat 用户环境变量文件
     elif [ -e "${HOME}/.bashrc" ]; then
-        source "${HOME}/.bashrc"
+        source "${HOME}/.bashrc"                                               # Debian、RedHat 用户环境变量文件
     fi
-    
-    source "/etc/profile"
+        
+    source "/etc/profile"                                                      # 系统环境变量文件路径
     
     echo "    ************************** 获取公共函数 **************************    "
     # shellcheck source=./common.sh
-    source "${ROOT_DIR}/bin/common.sh"
+    source "${ROOT_DIR}/bin/common.sh"                                         # 当前程序使用的公共函数
     
-    export -A PARAM_LIST=()
-    read_param "${ROOT_DIR}/conf/${CONFIG_FILE}"
+    export -A PARAM_LIST=()                                                    # 初始化 配置文件 参数
+    read_param "${ROOT_DIR}/conf/${CONFIG_FILE}"                               # 读取配置文件，获取参数
 }
 
 
@@ -45,7 +48,7 @@ function remove_key()
     expect -c \
     " 
         set timeout 30;
-        spawn ssh $1@$3 rm -rf ${HOME}/.ssh;
+        spawn ssh -p ${SSH_PORT} $1@$3 rm -rf ${HOME}/.ssh;
         expect {
             *yes/no*     { send  --  yes\r;  exp_continue; }
             *password*   { send  --  $2\r;   exp_continue; }
@@ -61,7 +64,7 @@ function create_keygen()
     expect -c \
     " 
         set timeout 30;
-        spawn ssh $1@$3 ssh-keygen -t rsa -P '' -f ${HOME}/.ssh/id_rsa;
+        spawn ssh -p ${SSH_PORT} $1@$3 ssh-keygen -t rsa -P '' -f ${HOME}/.ssh/id_rsa;
         expect {
             *Overwrite*  { send  --  y\r;    exp_continue; }
             *yes/no*     { send  --  yes\r;  exp_continue; }
@@ -78,7 +81,7 @@ function ssh_copy_id()
     expect -c \
     "
         set timeout 30;
-        spawn ssh $1@$4 ssh-copy-id -f $1@$3;              
+        spawn ssh -p ${SSH_PORT} $1@$4 ssh-copy-id -f $1@$3;              
         expect {
             *yes/no*    { send  --  yes\r; exp_continue; }
             *password*  { send  --  $2\r;  exp_continue; }  
@@ -94,7 +97,7 @@ function scp_copy_pub()
     expect -c \
     "
         set timeout 30;
-        spawn scp $1@$3:${HOME}/.ssh/id_rsa.pub ${HOME}/.ssh/id_rsa.pub.$3; 
+        spawn scp -P -p ${SCP_PORT} $1@$3:${HOME}/.ssh/id_rsa.pub ${HOME}/.ssh/id_rsa.pub.$3; 
         expect {
             *yes/no*    { send  --  yes\r; exp_continue; }
             *password*  { send  --  $2\r;  exp_continue; }    
@@ -110,7 +113,7 @@ function scp_copy_keys()
     expect -c \
     "
         set timeout -1;
-        spawn scp ${HOME}/.ssh/authorized_keys $1@$3:${HOME}/.ssh/;                              
+        spawn scp -P ${SCP_PORT} ${HOME}/.ssh/authorized_keys $1@$3:${HOME}/.ssh/;                              
         expect {
             *yes/no*    { send  --  yes\r; exp_continue; }
             *password* { send -- $2\r;  exp_continue; }  
@@ -123,7 +126,6 @@ function scp_copy_keys()
 # 合成秘钥
 function sync_keygen()
 {
-    # 定义局域变量
     local password host master h test_result count=0                           # 定义局域变量
     
     password=$(get_param "server.password")                                    # 获取主机密码
@@ -132,21 +134,21 @@ function sync_keygen()
     echo "    ************************ 生成 SSH 秘钥对 *************************    "
     for host in ${HOST_LIST}
     do
-        remove_key "${USER}" "${password}" "${host}"                           # 生成 ssh 秘钥对
-        create_keygen "${USER}" "${password}" "${host}"                        # 生成 ssh 秘钥对
+        remove_key    "${USER}" "${password}" "${host}"                        # 删除 SSH 秘钥对
+        create_keygen "${USER}" "${password}" "${host}"                        # 生成 SSH 秘钥对
     done
     
     echo "    *********************** 发送公钥到 Master ************************    "
     for host in ${HOST_LIST}
     do
         if [[ "${master}" != "${host}" ]]; then
-            ssh_copy_id   "${USER}" "${password}" "${master}" "${host}"        # 将秘钥 id 复制到 master
-            scp_copy_pub  "${USER}" "${password}" "${host}"                    # 将公钥发送到 master
+            ssh_copy_id   "${USER}" "${password}" "${master}" "${host}"        # 将秘钥 ID 复制到 Master
+            scp_copy_pub  "${USER}" "${password}" "${host}"                    # 将公钥发送到 Master
         fi
     done
      
     echo "    **************************** 合成公钥 ****************************    "
-    cat "${HOME}"/.ssh/id_rsa.pub*  >>  "${HOME}"/.ssh/authorized_keys         # 合成 公钥
+    cat "${HOME}"/.ssh/id_rsa.pub*  >>  "${HOME}/.ssh/authorized_keys"         # 合成 公钥
     
     echo "    ************************** 分发合成公钥 **************************    "
     for host in ${HOST_LIST}
@@ -161,7 +163,7 @@ function sync_keygen()
     do
         for h in ${HOST_LIST}
         do
-            ssh "${USER}@${host}" "echo '  <== ${host} 登录 ${h} ==>  '; exit"  >> "${HOME}/.ssh/test.log"
+            ssh -p "${SSH_PORT}" "${USER}@${host}" "echo '  <== ${host} 登录 ${h} ==>  '; exit"  >> "${HOME}/.ssh/test.log"
             (( count = count + 1 ))
         done
     done
